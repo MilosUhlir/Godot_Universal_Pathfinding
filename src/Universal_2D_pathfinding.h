17a6d998440a1f26e879661f61fbcdad2693de1a
MilosUhlir <milos.uhlir@volny.cz>
2024-10-31 18:22:23 +0100

    started adding parameters for the pathfinder

--- src/Universal_2D_pathfinding.h
+++ src/Universal_2D_pathfinding.h
@@ -9,7 +9,7 @@
 #include <vector>
 
 
-#define MAX_PATH_LENGTH 10000
+// #define MAX_PATH_LENGTH 10000
 
 
 namespace godot {
@@ -20,18 +20,36 @@ class Universal_2D_Pathinding : public Node {
 private:
 	std::vector<std::pair<int, int>> Path;
 
-    
+	int MAX_PATH_LENGTH;	// maximum possible length of path before pathfinder termination
+	// PathDiversion; // How far will the actual path be from the shortest path (in tiles?)
+	TileMapLayer TileMap;
+	enum SelectAlgorithm {
+		AStar,
+		DynamicPrograming,
+		Dijkstra
+	};
+	std::vector<Vector2i> Waypoints;
+	bool UseWaypoints;
+	enum WaypointOrder {
+		All_In_Order,
+		All_Random,
+		Single_random,
+		Single_In_Order
+	};
+	Vector2i HardEnd;
 
-    TileMapLayer Tilemap;
 
 protected:
 	static void _bind_methods();
+
 
 public:
 	Universal_2D_Pathinding();
 	~Universal_2D_Pathinding();
 
 	//void _process(double delta) override;
+	void _set(const property);
+	
 };
 
 }
