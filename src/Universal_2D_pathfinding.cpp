33548ad232a8d6778e33080159c6424ec4b95da9
MilosUhlir <milos.uhlir@volny.cz>
2025-02-10 12:19:58 +0100

    some stuff

--- src/Universal_2D_pathfinding.cpp
+++ src/Universal_2D_pathfinding.cpp
@@ -6,36 +6,117 @@
 using namespace godot;
 
 
-void Universal_2D_Pathinding::_bind_methods() {
+void Universal_2D_Pathinder::_bind_methods() {
 }
 
-Universal_2D_Pathinding::Universal_2D_Pathinding() {
+Universal_2D_Pathinder::Universal_2D_Pathinder() {
     // initialization of variables
+    Path;
+    MAX_PATH_LENGTH = 100000;
+    // PathDiversion
+    Map;
+    Map_tileset = Map.get_tile_set().operator->();
     
+
+
 }
 
 
-Universal_2D_Pathinding::~Universal_2D_Pathinding() {
+Universal_2D_Pathinder::~Universal_2D_Pathinder() {
     // cleanup
 }
 
 // Main methods
 
+    // Algorithms
+        // A*
+        std::vector<std::pair<int, int>> Universal_2D_Pathinder::AStar_Pathfinder(Vector2i start_node, Vector2i end_node) {
+
+            // 
+
+        }
+
+
+        // Dynamic Programming
+
+
+        // Dijkstra
+
+
+        // other...
+
+
+
+    // preprocessor
+    std::vector<std::vector<Universal_2D_Pathinder::Node_Data>> Universal_2D_Pathinder::Preprocessor() {
+        // Vector2i map_size = Vector2i(0,0), TileMapLayer Map
+        Vector2i start_coords(0,0);
+        int map_size_x = map_size[0];
+        int map_size_y = map_size[1];
+        for (int x = 0; x <= map_size_x; x++) {
+            for (int y = 0; y <= map_size_y; y++) {
+                Vector2i node_atlas_coords = Map.get_cell_atlas_coords(Vector2i(x,y));
+                Preprocessed_Map[x][y].Node_coordinates = Vector2i(x,y);
+                Preprocessed_Map[x][y].Node_cost;
+                Preprocessed_Map[x][y].Node_Label = 0;
+                Preprocessed_Map[x][y].Node_Neighbors;
+            }
+        }
+    }
 
 
 
 
+/*// getters and setters
 
-
-
-
-// getters and setters
     // MAX_PATH_LENGTH
-    void Universal_2D_Pathinding::set_MAX_PATH_LENGTH(const int new_max_length) {
+    void Universal_2D_Pathinder::set_MAX_PATH_LENGTH(const int new_max_length) {
         MAX_PATH_LENGTH = new_max_length;
     }
-    int Universal_2D_Pathinding::get_MAX_PATH_LENGTH() const {
+    int Universal_2D_Pathinder::get_MAX_PATH_LENGTH() const {
         return MAX_PATH_LENGTH;
     }
 
-    //
+    // path diversion
+
+
+	// tilemap
+	void Universal_2D_Pathinder::set_Map(const TileMapLayer new_Map) {
+        // Map = new_Map;
+    }
+	TileMapLayer Universal_2D_Pathinder::get_Map() const {
+        return Map;
+    }
+
+	// Waypoints
+	void Universal_2D_Pathinder::set_Waypoints(const std::vector<Vector2i> new_waypoints) {
+        Waypoints = new_waypoints;
+    }
+	std::vector<Vector2i> Universal_2D_Pathinder::get_Waypoints() const {
+        return Waypoints;
+    }
+
+	// useWaypoints
+	void Universal_2D_Pathinder::set_UseWaypoints(const bool new_UseWaypoints) {
+        UseWaypoints = new_UseWaypoints;
+    }
+	bool Universal_2D_Pathinder::get_UseWaypoints() const {
+        return UseWaypoints;
+    }
+
+	// HardEnd
+	void Universal_2D_Pathinder::set_HardEnd(const Vector2i new_HardEnd) {
+        HardEnd = new_HardEnd;
+    }
+	Vector2i Universal_2D_Pathinder::get_HardEnd() const {
+        return HardEnd;
+    }
+
+	// pathfinder
+	void Universal_2D_Pathinder::set_Pathfinder(const int new_pathfinder) {
+        Pathfinder = new_pathfinder;
+    }
+	int Universal_2D_Pathinder::get_Pathfinder() const {
+        return Pathfinder;
+    }
+    */
--- src/Universal_2D_pathfinding.h
+++ src/Universal_2D_pathfinding.h
@@ -7,6 +7,7 @@
 
 // my includes
 #include <vector>
+#include <map>
 
 
 // #define MAX_PATH_LENGTH 10000
@@ -14,30 +15,47 @@
 
 namespace godot {
 
-class Universal_2D_Pathinding : public Node {
-	GDCLASS(Universal_2D_Pathinding, Node)
+class Universal_2D_Pathinder : public Node {
+	GDCLASS(Universal_2D_Pathinder, Node)
 
 private:
-	// Variables
+	// internal variables
 	std::vector<std::pair<int, int>> Path;			// array (vector) of tile coordinates
-	int MAX_PATH_LENGTH;							// maximum possible length of path before pathfinder termination
-	// PathDiversion;								// How far will the actual path be from the shortest path (in tiles?)
-	TileMapLayer TileMap;							// TileMapLayer object to read Tilemap Data like tileset type and tile atlas
-	std::vector<Vector2i> Waypoints;				// an array of user defined waypoints which will be used to generate the path
-	bool UseWaypoints;								// whether to use waypoints or not
-	Vector2i HardEnd;								// an override of the pathfinding target location (e.g. for tower defense where you only need paths to one set point)
+	std::vector<std::pair<int, int>> SearchVector;	// array of neighbor tiles
+	TileSet* Map_tileset;							// TileSet object
+	Vector2i square_search_array[8];				// search array for square(and isometrtic) tile grids
+	Vector2i hex_search_array[6];					// search array for hexagonal tile grid
 
+	struct Node_Data {								// Structure to store all needed node data in one spot
+		Vector2i Node_coordinates;					// map coordinates of current node
+		int Node_cost;								// cost of movement onto this node
+		std::vector<Vector2i> Node_Neighbors; 		// array of coordinates of neighboring nodes
+		float Node_Label;							// the total cost to reach this node from start point
+		enum Node_state {
+			
+		};
+	};
+	
+	std::vector<std::vector<struct Node_Data>> Preprocessed_Map;
+	
+	
+
+	// accessible parameters
+	
+	
+	
 	// Methods
-
+	
 
 
 protected:
 	static void _bind_methods();
+	
 
 
 public:
 	// Variables
-	enum SelectAlgorithm {							// enum for selection of Pathfinding algorithm
+	enum Pathfinder {							// enum for selection of Pathfinding algorithm
 		AStar,
 		DynamicPrograming,
 		Dijkstra
@@ -48,19 +66,74 @@ public:
 		Single_random,
 		Single_In_Order
 	};
-
+	int MAX_PATH_LENGTH;							// maximum possible length of path before pathfinder termination
+	// PathDiversion;								// How far will the actual path be from the shortest path (in tiles?)
+	TileMapLayer Map;								// TileMapLayer object to read Map Data like tileset type and tile atlas
+	std::vector<Vector2i> Waypoints;				// an array of user defined waypoints which will be used to generate the path
+	bool UseWaypoints;								// whether to use waypoints or not
+	Vector2i HardEnd;								// an override of the pathfinding target location (e.g. for tower defense where you only need paths to one set point)
+	int Pathfinder;
+	Vector2i map_size = Vector2i(0,0);								// Coordinates of furthest tile from origin (0,0)
+	int map_size_x;									// X axis size of map
+	int map_size_y;									// Y axis size of map
+	Dictionary costs_dictionary;					// dictionary extracted from .JSON file containing all costs for each tile variant
+	
 
 	// Methods
 
 		// Constructor / Destructor
-		Universal_2D_Pathinding();
-		~Universal_2D_Pathinding();
+		Universal_2D_Pathinder();
+		~Universal_2D_Pathinder();
 	
-		// Setters / Getters
+		// Main methods
+			// Algorithms
+				// A*
+				std::vector<std::pair<int, int>> AStar_Pathfinder(Vector2i start_node, Vector2i end_node);
+				// Dynamic Programming
+
+				// Dijkstra
+
+				// other...
+			
+
+			// Preprocessor
+			std::vector<std::vector<struct Node_Data>> Preprocessor();
+
+
+		// Helper methods
+			// label calculation
+
+			// neighbor search
+
+		
+		/*// Setters / Getters
 			// MAX_PATH_LENGTH
 			void set_MAX_PATH_LENGTH(const int new_max_length);
 			int get_MAX_PATH_LENGTH() const;
-	
+
+			// path diversion
+
+
+			// Map
+			void set_Map(const TileMapLayer new_Map);
+			TileMapLayer get_Map() const;
+
+			// Waypoints
+			void set_Waypoints(const std::vector<Vector2i> new_waypoints);
+			std::vector<Vector2i> get_Waypoints() const;
+
+			// useWaypoints
+			void set_UseWaypoints(const bool new_UseWaypoints);
+			bool get_UseWaypoints() const;
+
+			// HardEnd
+			void set_HardEnd(const Vector2i new_HardEnd);
+			Vector2i get_HardEnd() const;
+
+			// pathfinder
+			void set_Pathfinder(const int new_pathfinder);
+			int get_Pathfinder() const;
+		*/
 };
 
 }
